e19183097bd5eb0b6550790462f6139fa677ae14
Lesego Sambo <admin@kewaa.co>
2022-09-02 11:15:22 +0200

    SENT

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -36,49 +36,29 @@ const Banner = () => {
               <div className="row">
                 <div className="col-lg-3 col-sm-3 col-6 text-center mb-20">
                   <div className="d-inline-block text-start">
-                    <h4 className="color-white"> <span className="count">265</span><span> K+</span></h4>
-                    <p className="font-sm color-text-mutted">Daily Jobs Posted</p>
+                    <h4 className="color-white"> <span className="count">2650</span></h4>
+                    <p className="font-sm color-text-mutted">Jobs Posted</p>
                   </div>
                 </div>
                 <div className="col-lg-3 col-sm-3 col-6 text-center mb-20">
                   <div className="d-inline-block text-start">
-                    <h4 className="color-white"><span className="count">17</span><span>  K+</span></h4>
+                    <h4 className="color-white"><span className="count">1700</span></h4>
                     <p className="font-sm color-text-mutted">Recruiters</p>
                   </div>
                 </div>
                 <div className="col-lg-3 col-sm-3 col-6 text-center mb-20">
                   <div className="d-inline-block text-start">
-                    <h4 className="color-white"> <span className="count">15</span><span>  K+</span></h4>
+                    <h4 className="color-white"> <span className="count">1500</span></h4>
                     <p className="font-sm color-text-mutted">Freelancers</p>
                   </div>
                 </div>
                 <div className="col-lg-3 col-sm-3 col-6 text-center mb-20">
                   <div className="d-inline-block text-start">
-                    <h4 className="color-white"> <span className="count">28</span><span> K+</span></h4>
+                    <h4 className="color-white"> <span className="count">280</span></h4>
                     <p className="font-sm color-text-mutted">Blog Tips</p>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
-        <div className="container">
-          <div className="list-brands mt-40 mb-30">
-            <div className="box-swiper">
-              <div className="swiper-container swiper-group-9 swiper swiper-initialized swiper-horizontal swiper-pointer-events">
-                <div className="swiper-wrapper" id="swiper-wrapper-c6299fbcb23610d22" aria-live="off" style={{transform: `translate3d(-2841.56px, 0px, 0px)`, transitionDuration: `0ms`}}><div className="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="1" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="2 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/sony.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="2" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="3 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/acer.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="3" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="4 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/nokia.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="4" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="5 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/assus.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="5" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="6 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/casio.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="6" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="7 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/dell.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="7" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="8 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/panasonic.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="8" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="9 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/vaio.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-prev" data-swiper-slide-index="9" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="10 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/sharp.svg" alt="jobBox"/></a></div>
-                  <div className="swiper-slide swiper-slide-duplicate-active" data-swiper-slide-index="0" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="1 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/microsoft.svg" alt="jobBox"/></a></div>
-                  <div className="swiper-slide swiper-slide-duplicate-next" data-swiper-slide-index="1" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="2 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/sony.svg" alt="jobBox"/></a></div>
-                  <div className="swiper-slide" data-swiper-slide-index="2" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="3 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/acer.svg" alt="jobBox"/></a></div>
-                  <div className="swiper-slide" data-swiper-slide-index="3" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="4 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/nokia.svg" alt="jobBox"/></a></div>
-                  <div className="swiper-slide" data-swiper-slide-index="4" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="5 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/assus.svg" alt="jobBox"/></a></div>
-                  <div className="swiper-slide" data-swiper-slide-index="5" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="6 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/casio.svg" alt="jobBox"/></a></div>
-                  <div className="swiper-slide" data-swiper-slide-index="6" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="7 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/dell.svg" alt="jobBox"/></a></div>
-                  <div className="swiper-slide" data-swiper-slide-index="7" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="8 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/panasonic.svg" alt="jobBox"/></a></div>
-                  <div className="swiper-slide" data-swiper-slide-index="8" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="9 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/vaio.svg" alt="jobBox"/></a></div>
-                  <div className="swiper-slide swiper-slide-prev" data-swiper-slide-index="9" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="10 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/sharp.svg" alt="jobBox"/></a></div>
-                <div className="swiper-slide swiper-slide-duplicate swiper-slide-active" data-swiper-slide-index="0" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="1 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/microsoft.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate swiper-slide-next" data-swiper-slide-index="1" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="2 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/sony.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="2" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="3 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/acer.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="3" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="4 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/nokia.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="4" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="5 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/assus.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="5" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="6 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/casio.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="6" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="7 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/dell.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="7" style={{width: `129.556px`, marginRight: `20px`}} role="group" aria-label="8 / 10"><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/panasonic.svg" alt="jobBox"/></a></div><div className="swiper-slide swiper-slide-duplicate" data-swiper-slide-index="8" role="group" aria-label="9 / 10" style={{width: `129.556px`, marginRight: `20px`}}><a href="#"><img src="http://wp.alithemes.com/html/jobbox/demos/assets/imgs/slider/logo/vaio.svg" alt="jobBox"/></a></div></div>
-              <span className="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
             </div>
           </div>
         </div>
